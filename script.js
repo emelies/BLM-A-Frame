e3bd40a187b1b5ede01d646760f3f67338cb86fc
Glitch (a-frame-explore) <none>
2020-06-04 09:42:15 +0000

    💅🖼 Checkpoint ./aframe-playvideo.js:1106040/321 ./index.html:1106040/4887 ./style.css:1106040/948 ./script.js:1106040/6208

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+window.onload = init;
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function init() {
+  document.querySelectorAll("a-box").forEach(box => {
+    box.onclick = function() {
+      let id = box.getAttribute("src");
+      let video = document.querySelector(id);
+      let src = video.src;
+    };
+  });
+}
+
+document.querySelectorAll("video").forEach(video => {
+  video.setAttribute("preload", true);
+  video.setAttribute("autoplay", true);
+  video.playsinline = true;
+  video.muted = false;
+  video.setAttribute("crossorigin", "anonymous");
+  video.play();
+  video.pause();
+});
